8a3bd1464148238511500211887b380fcb83d2f1
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-04 18:16:07 +0100

    script update

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const request1 = `${startpoint}media?fields=${fields}&access_token=${accessToken
 let posts = {}
 const numberOfPosts = 12
 const instagramTiles = document.querySelectorAll('div.instagram-grid div.grid-item')
+
 console.log(instagramTiles)
 
 // gets a list of posts
