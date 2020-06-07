15c48fc5dc01b35d5e4211370d09c548495a165c
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-07 12:48:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ console.log("hi");
 
 const list = document.querySelector('ul.list')
 const account = "https://www.instagram.com/_foodandotherstories"
+const instagramTiles = document.querySelectorAll('div.grid-item')
 const instagramFeed = true
 
 const getEdges = function() {
-  fetch(account + "/?__a=1")
+  return fetch(account + "/?__a=1")
     .then(response => response.json())
     .then(result => {
       let edges = result.graphql.user.edge_owner_to_timeline_media.edges;
       edges.forEach(edge => {
-        console.log(edge);
         edge = {
           url: edge.node.display_url,
           caption: edge.node.edge_media_to_caption.edges[0].node.text
         }
-        
+        console.log(edge)
       });
-      console.log(edges);
+      console.log('processed edges =  ', edges);
       return edges
     });
 };
