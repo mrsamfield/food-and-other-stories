2e807064676e37b5d93f40deca82e189a5f402c0
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-07 15:52:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const lockPreview = function (e) {
   if (caption) {
     captionPreview.innerHTML = caption
     captionPreview.style.left = (x - (captionPreview.getBoundingClientRect().width / 2))  + 'px'
-    captionPreview.style.top = y - 10 + 'px'
+    captionPreview.style.top = y - 10 + window.pageYOffset + 'px'
   } else {
     captionPreview.innerHTML = ""
   } 
