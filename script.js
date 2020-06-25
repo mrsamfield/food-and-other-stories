910960040ec7816de895ce9c97e687e23d634d7b
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-25 16:06:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import documentToHtmlString from '/node_modules/@contentful/rich-text-html-renderer/dist/rich-text-html-renderer.es5.js';
+import * as documentToHtmlString from '/node_modules/@contentful/rich-text-html-renderer/dist/rich-text-html-renderer.es5.js';
 console.log("hi");
 
 const list = document.querySelector('ul.list')
