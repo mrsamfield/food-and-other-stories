55db982eb608d6ad58aaa51282989c94bc949c3d
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-25 17:31:50 +0100

    trying to get this library to work!!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { documentToHtmlString } from '/node_modules/@contentful/rich-text-html-renderer/dist/rich-text-html-renderer.es5.js';
+import documentToHtmlString from '/node_modules/@contentful/rich-text-html-renderer/dist/rich-text-html-renderer.es5.js';
 
 console.log("hi");
 
