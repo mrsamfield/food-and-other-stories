05f76e7206cd169d3b53ff63f87df90fc3a59c02
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-26 13:54:29 +0100

    fix?

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-import { documentToHtmlString } from '/@contentful/rich-text-html-renderer';
+import * as contentful from 'contentful';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 console.log("hi");
 
