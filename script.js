d144756c7caac356605bf23fe3b698772cda6e05
mrsamfield <mrsamfield@hotmail.co.uk>
2020-06-30 11:58:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const connectImage = function (ID, assets) {
 
 
 // run grabData on load
-let content = grabContent()
+let posts = grabContent()
 
 //on clicking 
 
@@ -181,4 +181,4 @@ gridFill()
 //Blog post tester
 const blogContentTag = document.querySelector('div.blog-content')
 
-blogContentTag.innerHTML = content[0]
+blogContentTag.innerHTML = posts[0]
