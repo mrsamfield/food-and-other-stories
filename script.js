4629c9612a456b3e79fe9334345325f98fd8dffb
mrsamfield <mrsamfield@hotmail.co.uk>
2020-07-02 13:57:37 +0100

    fix line breaks

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ const grabContent = async function() {
       title: post.fields.title,
       keyImage: connectImage(post.fields.keyImage.sys.id, assets),
       sampleQuote: post.fields.sampleQuote,
-      blogBody: converter.makeHtml(post.fields.blogBody),
+      blogBody: converter.makeHtml(post.fields.blogBody).replace(/\n/g, "<br />"),
       color: post.fields.colour
     };
 
